a47ab26c9de1e7084b8bcabd1011c2125e0ce527
Bashar Dev <beelwarad52@gmail.com>
2023-05-28 18:33:57 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState({
+    id: '',
+    name: '',
+    email: '',
+    password: '',
+  });
+  const [userList, setUserList] = useState([]);
+
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  // @desc       GET all users
+  const getAllUsers = async () => {
+    const data = await fetch(`http://localhost:5000/data`)
+      .then((response) => response.json())
+      .catch((error) => console.log(error));
+    setUserList(data);
+  };
+
+  // @desc       POST create user
+  const addUser = async () => {
+    const data = await fetch(`http://localhost:5000/data`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => response.json())
+      .catch((error) => console.log(error));
+    setUserList(data);
+    getAllUsers();
+  };
+
+  // @desc       DELETE single user
+  const deleteSingleUser = async () => {
+    await fetch(`http://localhost:5000/data/${user?.id}`, {
+      method: 'DELETE',
+    })
+      .then((response) => response.json())
+      .catch((error) => console.log(error));
+    getAllUsers();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <main>
+        <section>
+          <form action='submit'>
+            <label htmlFor='userIdIn'>ID</label>
+            <input
+              id='userIdIn'
+              name='id'
+              type='number'
+              value={user.id}
+              onChange={handleChange}
+            />
+            <label htmlFor='name'>Name</label>
+            <input
+              type='text'
+              name='name'
+              id='name'
+              value={user.name}
+              onChange={handleChange}
+            />
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              name='email'
+              id='email'
+              value={user.email}
+              onChange={handleChange}
+            />
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              name='password'
+              id='password'
+              value={user.password}
+              onChange={handleChange}
+            />
+          </form>
+        </section>
+        <section>
+          <button id='getAllUsers' onClick={getAllUsers}>
+            Get all users
+          </button>
+          <button id='addUser' onClick={addUser}>
+            Crate a user
+          </button>
+          <button id='deleteSingleUser' onClick={deleteSingleUser}>
+            Delete a user
+          </button>
+        </section>
+        <section>
+          <ul id='usersList'>
+            {userList.user?.map((e) => (
+              <li key={e.id}>{`${e.name}:  ${e.email}`}</li>
+            ))}
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
-
 export default App;
